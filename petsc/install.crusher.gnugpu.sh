28fe94e4d53bc5112136e674f8a0228789ed3343
Gautam Bisht <gbisht@lbl.gov>
2023-04-28 18:32:04 -0400

    Updates files for Crusher

--- petsc/install.crusher.gnugpu.sh
+++ petsc/install.crusher.gnugpu.sh
@@ -3,9 +3,9 @@
 source ../modules/modules.crusher.gnugpu
 
 ./configure \
---with-cc=/opt/cray/pe/craype/2.7.16/bin/cc \
---with-cxx=/opt/cray/pe/craype/2.7.16/bin/CC \
---with-fc=/opt/cray/pe/craype/2.7.16/bin/ftn \
+--with-cc=/opt/cray/pe/craype/2.7.19/bin/cc \
+--with-cxx=/opt/cray/pe/craype/2.7.19/bin/CC \
+--with-fc=/opt/cray/pe/craype/2.7.19/bin/ftn \
 --with-fortran-bindings=1 \
 --with-mpiexec="srun -g 8 --smpiargs=-gpu " \
 --with-batch=0 \
@@ -13,9 +13,9 @@ source ../modules/modules.crusher.gnugpu
 --download-kokkos-kernels \
 --with-kokkos-kernels-tpl=0 \
 --with-make-np=8 \
---with-netcdf-dir=/opt/cray/pe/netcdf-hdf5parallel/4.8.1.1/gnu/9.1 \
---with-pnetcdf-dir=/opt/cray/pe/parallel-netcdf/1.12.1.7/gnu/9.1 \
---with-hdf5-dir=/opt/cray/pe/hdf5-parallel/1.12.1.1/gnu/9.1 \
+--with-netcdf-dir=/opt/cray/pe/netcdf-hdf5parallel/4.9.0.1/gnu/9.1 \
+--with-pnetcdf-dir=/opt/cray/pe/parallel-netcdf/1.12.3.1/gnu/9.1 \
+--with-hdf5-dir=/opt/cray/pe/hdf5-parallel/1.12.2.1/gnu/9.1 \
 --with-hip=1 \
 --with-hipc=/opt/rocm-5.4.0/bin/hipcc \
 --download-parmetis \
